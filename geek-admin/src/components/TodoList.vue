4374d357455d7583b55a5741748458599110e487
Jacky Song <jacky_song@epam.com>
2022-03-08 16:53:50 +0800

    feat: add chap06 demo

---
+++ geek-admin/src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input v-model="title" type="text" @keydown.enter="addTodo"/>
+    <button v-if="active < all" @click="clear">清理</button>
+    <ul v-if="todos.length">
+      <li v-for="todo in todos">
+        <input v-model="todo.done" type="checkbox"/>
+        <span :class="{ done: todo.done }"> {{ todo.title }}</span>
+      </li>
+    </ul>
+    <div v-else>暂无数据</div>
+    <div>
+      全选<input v-model="allDone" type="checkbox"/>
+      <span> {{ active }} / {{ all }} </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import useTodos from '../utils/useTodos'
+
+let {title, todos, addTodo, clear, active, all, allDone} = useTodos();
+</script>
--- geek-admin/src/pages/home.vue
+++ geek-admin/src/pages/home.vue
@@ -1,3 +1,19 @@
 <template>
   <h1>这是首页</h1>
+  <Count/>
+  <TodoList/>
 </template>
+
+<script setup>
+import TodoList from '../components/TodoList.vue'
+import Count from '../components/Count.vue'
+import {useMouse} from '../utils/mouse'
+
+let {x, y} = useMouse()
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>>
